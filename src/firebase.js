72d04cf7ebe258250cf0a2019704bce0c479ae9d
Laura <laura.coombs07@gmail.com>
2018-12-13 13:26:32 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+  const config = {
+    apiKey: "AIzaSyAVuMobFaaCFHKqXnu56ZeZjY3ebz0cI3o",
+    authDomain: "fun-foods-friends-ebf72.firebaseapp.com",
+    databaseURL: "https://fun-foods-friends-ebf72.firebaseio.com",
+    projectId: "fun-foods-friends-ebf72",
+    storageBucket: "fun-foods-friends-ebf72.appspot.com",
+    messagingSenderId: "955295202565"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
